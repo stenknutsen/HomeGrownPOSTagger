c02951fd3f040bb2029908ac5a7a46ddddc3e012
stenknutsen <stenknutsen@stens-MacBook-Pro.local>
2015-11-24 20:50:42 -0500

    Added more functions
    Coordinating conjunctions and a few single prepositions.

--- TeenieTinyPOSTagger.py
+++ TeenieTinyPOSTagger.py
@@ -3,8 +3,10 @@ import headers
 test = ['In', 'fact', ',', 'when', 'he', 'was', 'questioned', 'by', 'ABC', 'News', 'today', 'about', 'his', 'assertion', 'that', '``', 'thousands', 'and', 'thousands', 'of', 'people', "''", 'cheered', 'the', 'collapse', 'of', 'the', 'World', 'Trade', 'Center', 'on', 'Sept.', '11', 'just', 'across', 'the', 'river', 'in', 'New', 'Jersey', ',', 'Trump', 'doubled', 'down', '.']
 #test = ['Republican', 'presidential', 'candidate', 'Donald', 'Trump', 'speaks', 'during', 'a', 'campaign', 'stop', 'on', 'Saturday', 'in', 'Birmingham']
 tinyDTDict = {"a":"DT","the":"DT","this":"DT","these":"DT","those":"DT","my":"DT", "your":"DT", "his":"DT", "her":"DT", "its":"DT", "our":"DT","their":"DT","some":"DT"}
-
-def NNPFinder(sent):
+tinyCCDict = {"and":"CC", "or":"CC","but":"CC","&":"CC"}
+tinyINDict = {"in":"IN","by":"IN","of":"IN"}
+#Tags anything starting with a captial letter (ecluding first word in sentence) as NNP. Also adds UNK.<==will probably change this
+def NNPTagger(sent):
     iterSent = iter(sent)
 
     sentToReturn = []
@@ -22,23 +24,51 @@ def NNPFinder(sent):
     return sentToReturn
 
 
-
-def DTFinder(sent):
+#Tags determiners
+def DTTagger(sent):
     sentToReturn = []
     for word in sent:
-        if word[0] in  tinyDTDict:
+        if word[0].lower() in tinyDTDict:
             newTup = (word[0],'DT')
             sentToReturn += [newTup]
         else:
             sentToReturn += [word]
-
-
-
     return sentToReturn
 
-findnnps = NNPFinder(test)
+#tags very limited coordinating conjuncts
+def CCTagger(sent):
+    sentToReturn = []
+    for word in sent:
+        if word[0].lower() in tinyCCDict:
+            newTup = (word[0],'CC')
+            sentToReturn += [newTup]
+        else:
+            sentToReturn += [word]
+    return sentToReturn
+
+def INTagger(sent):
+    sentToReturn = []
+    for word in sent:
+        if word[0].lower() in tinyINDict:
+            newTup = (word[0],'IN')
+            sentToReturn += [newTup]
+        else:
+            sentToReturn += [word]
+    return sentToReturn
+
+
+
+findnnps = NNPTagger(test)
 
 print(findnnps)
-findnnps = DTFinder(findnnps)
+findnnps = DTTagger(findnnps)
+
+print(findnnps)
+
+findnnps = CCTagger(findnnps)
+
+print(findnnps)
+
+findnnps = INTagger(findnnps)
 
 print(findnnps)
